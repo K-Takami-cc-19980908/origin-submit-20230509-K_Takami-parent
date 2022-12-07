cea9be39318e5b538ec29ce2bada0b5dbfa5cd42
NNakamura <natsuki.nakamura.carecon@gmail.com>
2022-12-07 15:08:34 +0900

    Laravelのwork調整

--- Laravel/src/routes/web.php
+++ Laravel/src/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// 管理画面
+Route::group(['prefix' => '/admin', 'as' => 'admin.'], function(){
+  // 管理画面トップ
+  Route::get('/', 'admin\AdminController@index')->name('index');
+  // 商品登録画面
+  Route::get('/product/add', 'admin\ProductController@add')->name('product.add');
 });
